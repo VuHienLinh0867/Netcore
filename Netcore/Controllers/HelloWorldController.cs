8c3de8c09ee65f3afe1d3e1cd9be3029460aec9e
Vu Hien Linh <hienlinh0211@gmail.com>
2021-11-18 02:07:23 +0700

    Demo Controller

--- Netcore/Controllers/HelloWorldController.cs
+++ Netcore/Controllers/HelloWorldController.cs
@@ -1,23 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Encodings.Web;
 
-namespace MvcMovie.Controllers
+namespace DEMONETCORE.Controllers
 {
     public class HelloWorldController : Controller
     {
         // 
         // GET: /HelloWorld/
 
-        public IActionResult Index()
+        public string Index()
         {
-            return View();
+            return "This is my default action...";
         }
-        public IActionResult Welcome(string name, int numTimes = 1)
-        {
-            ViewData["Message"] = "Hello " + name;
-            ViewData["NumTimes"] = numTimes;
 
-            return View();
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        // GET: /HelloWorld/Welcome/ 
+        // Requires using System.Text.Encodings.Web;
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
         }
     }
 }
